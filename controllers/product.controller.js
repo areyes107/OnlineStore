a0258ddaa90d0e32f90e76ed408721e2bd4f35c1
areyes107 <angelmrg2000@gmail.com>
2020-03-09 15:19:14 -0600

    controllers/product.controller:

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -32,6 +32,74 @@ function saveProduct (req, res){
     }
 }
 
+function listProducts (req, res){
+    Product.find({}, (err, productFind)=>{
+        if(err){
+             res.status(500).send({message: 'Error en el servidor'});
+        }else if(productFind){
+            res.send({products: productFind});
+        }else{
+            res.status(404).send({message: 'No se encontró ningún producto'});
+        }
+    })
+}
+
+function stockProducts(req, res){
+    const productId = req.params.id;
+
+    Product.findById(productId, (err, stockT)=>{
+        if(err){
+            res.status(500).send({message: 'Error en el servidor'});
+        }else if(stockT){
+            res.send({product: stockT.name, Available: stockT.quantity});
+        }else{
+            res.status(404).send({message: 'No se encontró ningun empleado'})
+        }
+    })  
+}
+
+function updateProduct(req, res){
+    var productId = req.params.id;
+    var update = req.body;
+
+    Product.findOne({name: update.name},  (err, productFind)=>{
+        if(err){
+            res.status(500).send({message: 'Error en el servidor'});
+        }else if(productFind){
+            res.status(418).send({message: 'El producto ya existe'});
+
+        }else{
+            Product.findByIdAndUpdate(productId, update, {new: true}, (err, productUpdated)=>{
+                if(err){
+                    res.status(500).send({message: 'Error en el servidor'});
+                }else if(productUpdated){
+                    res.send({product: productUpdated});
+                }else{
+                    res.status(404).send({message: 'No se pudo actualizar el producto'});
+                }
+            })
+        }
+    })
+}
+
+function deleteProduct(req, res){
+    var productId = req.params.id;
+
+    Product.findByIdAndDelete(productId, (err, productDeleted)=>{
+        if(err){
+            res.status(500).send({message: 'Error en el servidor'});
+        }else if(productDeleted){
+            res.send({message: 'Producto eliminado correctamente', productDeleted})
+        }else{
+            res.status(404).send({message: 'No se pudo borrar el producto'});
+        }
+    })
+}
+
 module.exports = {
-    saveProduct
+    saveProduct,
+    listProducts,
+    stockProducts,
+    updateProduct,
+    deleteProduct
 }
