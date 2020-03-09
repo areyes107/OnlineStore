8f2f1383abac53d8b61e24910f62df1efd72ff44
areyes107 <angelmrg2000@gmail.com>
2020-03-09 15:19:15 -0600

    routes/product.route:

--- routes/product.route.js
+++ routes/product.route.js
@@ -5,5 +5,9 @@ var api = express();
 var productController = require('../controllers/product.controller');
 
 api.post('/saveProduct', productController.saveProduct);
+api.get('/listProducts', productController.listProducts);
+api.get('/stockProducts/:id', productController.stockProducts);
+api.put('/updateProduct/:id', productController.updateProduct);
+api.delete('/deleteProduct/:id', productController.deleteProduct);
 
 module.exports = api;
