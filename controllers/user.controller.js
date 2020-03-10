b46d0da374d9105c09949ca0c6dbce9786c0593f
areyes107 <angelmrg2000@gmail.com>
2020-03-09 22:03:15 -0600

    controllers/user.controller:

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,117 @@
 'use strict'
 
+var User = require('../models/user.model');
+var bcrypt = require('bcrypt-nodejs');
+var jwt = require ('../services/jwt');
 
-const { Rol } = require('../enums');
+function saveUser (req, res){
+    var params = req.body;
+    var user = new User();
+
+    if(params.name && 
+        params.username &&
+        params.password){
+            User.findOne({$or: [{username: params.username}, {name: params.name}]}, (err, userFind)=>{
+                if(err){
+                    res.status(500).send({message: 'Error en el servidor'});
+
+                }else if(userFind){
+                    res.send({message: 'usuario o nombre ya usado'});
+
+                }else{
+                    user.name = params.name;
+                    user.username = params.username;
+                    user.password = params.password;
+                    user.email = params.email;
+
+                    bcrypt.hash(params.password, null, null, (err, hashPassword)=>{
+                        if(err){
+                            res.status(500).send({message: 'Error de encriptación'});
+                        }else{ 
+
+                            user.password = hashPassword;
+                            user.save((err, userSaved)=>{
+                                if(err){
+                                    res.status(500).send({message: 'Error en el servidor'});
+                                }else if(userSaved){
+                                    res.send({user: userSaved});
+                                }else{
+                                    res.status(418).send({message: 'Usuario no guardado'});
+        
+                                }
+                            });  
+                        }
+                    });
+                }});
+        }else{
+            res.send({message: 'ingrese todos los datos'});
+        }
+}
+
+function updateRole (req, res){
+    var userId = req.params.id;
+    var params = req.body;
+    User.findByIdAndUpdate(userId, params, {new: true}, (err, userUpdated)=>{
+        if(err){
+            res.status(500).send({message: 'Error en el servidor'});
+        }else if(userUpdated){
+            res.send({user: userUpdated});
+        }else{
+            res.status(418).send({message: 'Admin no actualizado'});
+        }
+    })
+}
+
+function updateClient (req, res){
+    var userId = req.params.id;
+    var params = req.body;
+        User.findByIdAndUpdate(userId, params, {new: true}, (err, userUpdated)=>{
+            if(err){
+                res.status(500).send({message: 'Error en el servidor'});
+            }else if(userUpdated){
+                if(userUpdated.role == 'ADMIN'){
+                    res.send({user: userUpdated});
+                }else{
+                    res.send({message: 'No puede actualizar un usuario ADMIN'});
+                }
+            }else{
+                res.status(418).send({message: 'Admin no actualizado'});
+            }
+        })
+}
+
+function deleteClient (req, res){
+
+    User.findById(userId, (err, userFind)=>{
+        if(err){
+            res.status(500).send({message: 'Error en el servidor'});
+        }else if(userFind){
+            if(userFind.role == 'CLIENT'){
+                User.findByIdAndDelete(userId, (err, userDeleted)=>{
+                    if(err){
+                        res.status(500).send({message: 'Error en el servidor'});
+                    }else{
+                        if(userDeleted){
+                            res.status(200).send({message: 'Registro Eliminado'});
+            
+                        }else{
+                            res.status(404).send({message: 'Error al encontrar el registro'});
+                        }
+                    }
+                });
+            }else{
+                res.send({message: 'No puede eliminar un admin'});
+            }
+        }else{
+            res.send({message: 'No se encontró el usuario'});
+        }
+    })
+    
+}
+
+module.exports ={
+    saveUser,
+    updateRole,
+    updateClient,
+    deleteClient
+}
