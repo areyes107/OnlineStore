fd33feb3c0a7c45748ffc729f2e0573d984d4033
areyes107 <angelmrg2000@gmail.com>
2020-03-11 22:48:41 -0600

    routes/bill.route:

--- routes/bill.route.js
+++ routes/bill.route.js
@@ -5,6 +5,7 @@ var billController = require('../controllers/bill.controller');
 var api = express();
 var middlewareAuth = require('../middlewares/authenticated');
 
-api.post('/createBill/:id', billController.createBill);
+api.post('/createBill/:id', middlewareAuth.ensureAuthClient, billController.createBill);
+api.get('/showBill/:id', middlewareAuth.ensureAuthClient, billController.createBill);
 
 module.exports = api;
